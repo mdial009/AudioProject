26014c7598e0dcb4ebfe450edf3afe6fdaa9ea02
Madany Diallo <madanydiallo@Madanys-MacBook-Pro.local>
2020-05-25 15:46:18 -0400

    Initial Commit

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+import math
+import os
+import sys
+
+import requests
+
+print(sys.version)
+# print((sys.version))
+# print(sys.version_info)
+print(sys.executable)
+print("hello world")
+
+r = requests.get("https://google.com")
+print(r.status_code)
+
+name = input("Madany Put Your Name: ")
+print("hello", name)
